98fedaddfef4817c03279be22efed9a41aa8dbef
Ben <b.harris@capital-iom.com>
2024-01-15 14:16:08 +0000

    New

--- baseline-adminAccountHardcode.sh
+++ baseline-adminAccountHardcode.sh
@@ -16,13 +16,11 @@ fi
 # Variables #
 #############
 
-variable1="Blu3Duck4U2?1901"
+username=$1
+password=$2
 
 # Path to Baseline Scripts directory
 BaselineScripts="/usr/local/Baseline/Scripts"
-
-# Create admin user
-username=admin.cig
 
 ######################
 # Script Starts Here #
@@ -36,5 +34,5 @@ dscl . -create /Users/$username RealName "Administrator Account"
 dscl . -create /Users/$username UniqueID "510"
 dscl . -create /Users/$username PrimaryGroupID 20
 dscl . -create /Users/$username NFSHomeDirectory /Users/$username
-dscl . -passwd /Users/$username $variable1
+dscl . -passwd /Users/$username $password
 dscl . -append /Groups/admin GroupMembership $username
